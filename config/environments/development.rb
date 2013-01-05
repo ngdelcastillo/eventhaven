ddef45ab6ea200021241556f495fa6fd40b91382
Noel del Castillo <ngdelcastillo@gmail.com>
2013-01-05 21:06:39 +0800

    added bootstrap and devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,20 @@ Gimika::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:5000' }
+  config.action_mailer.delivery_method = :smtp
+  # change to false to prevent email from being sent during development
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "devaidacademy",
+    password: "devmachine"
+  }
 end
