cfefe05cdf117317799f2c0a402fa863a59e3811
Noel del Castillo <ngdelcastillo@gmail.com>
2013-01-05 10:04:51 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gimika::Application.config.secret_token = '967f7d1e1dd80f8966c9edd765461fd5408bd37134fdd471f356c02e7d68a6a67a080062bce2acc818eb3db6368b4f6801f3409e174de33baffaf0efec0707ae'
